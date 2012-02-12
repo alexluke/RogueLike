bcb8a6a64004317b0966d3fd85319580c87d5a8f
Alex Luke <banduin@gmail.com>
2012-02-12 10:46:17 -0800

    Corridors to connect rooms

---
+++ RogueLike/RogueLike/Corridor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace RogueLike
+{
+	public class Corridor
+	{
+		public Vector2 Start { get; set; }
+		public Vector2 End { get; set; }
+
+		public Corridor(Vector2 start, Vector2 end)
+		{
+			Start = start;
+			End = end;
+		}
+
+		public void Draw(WorldCanvas canvas)
+		{
+			if (Start.X == End.X)
+			{
+				// vertical line
+				var x = (int)Start.X;
+				for (var y = (int)Start.Y; y < (int)End.Y; y++)
+				{
+					canvas.PlaceTile(WorldCanvas.Tile.Wall, x - 1, y);
+					canvas.PlaceTile(WorldCanvas.Tile.Floor, x, y);
+					canvas.PlaceTile(WorldCanvas.Tile.Wall, x + 1, y);
+				}
+			}
+			else if (Start.Y == End.Y)
+			{
+				// horrizontal line
+				var y = (int)Start.Y;
+				for (var x = (int)Start.X; x < (int)End.X; x++)
+				{
+					canvas.PlaceTile(WorldCanvas.Tile.Wall, x, y - 1);
+					canvas.PlaceTile(WorldCanvas.Tile.Floor, x, y);
+					canvas.PlaceTile(WorldCanvas.Tile.Wall, x, y + 1);
+				}
+			}
+			else
+			{
+				throw new ArgumentOutOfRangeException("Corridor not in a straight line.");
+			}
+		}
+	}
+}
--- RogueLike/RogueLike/Level.cs
+++ RogueLike/RogueLike/Level.cs
@@ -9,17 +9,24 @@ namespace RogueLike
 	public class Level
 	{
 		public List<Room> Rooms { get; private set; }
+		public List<Corridor> Corridors { get; private set; }
 
 		public Level()
 		{
 			Rooms = new List<Room>();
+			Corridors = new List<Corridor>();
 			Rooms.Add(new Room(new Rectangle(5, 8, 50, 30)));
 			Rooms.Add(new Room(new Rectangle(60, 10, 20, 40)));
+			Rooms.Add(new Room(new Rectangle(2, 60, 20, 10)));
+
+			Corridors.Add(new Corridor(new Vector2(54, 20), new Vector2(61, 20)));
+			Corridors.Add(new Corridor(new Vector2(12, 37), new Vector2(12, 61)));
 		}
 
 		public void Draw(WorldCanvas canvas)
 		{
 			Rooms.ForEach(r => r.Draw(canvas));
+			Corridors.ForEach(c => c.Draw(canvas));
 		}
 	}
 }
